c70cda8e4125ef11512c55d874f1dd671186e5ab
Thomas <thomas_dvlper@hotmail.com>
2021-12-24 20:48:05 +0100

    Setup
    The initial project setup

---
+++ groceryApp/groceryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  groceryApp
+//
+//  Created by mac on 24/12/2021.
+//  Copyright © 2021 Thomas Shamoian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
